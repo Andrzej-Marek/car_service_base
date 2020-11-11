84d5a94dadbff5ec817a6c9103328a60841e91c0
Andrzej Marek <a.marek@fireup.pro>
2020-11-11 16:57:41 +0100

    Added sample code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React, { FC } from "react";
+import styled from "styled-components";
+import { PrimaryButton } from "./components";
+import { GlobalStyle } from "./shared/styles";
 
 interface OwnProps {}
 
 type Props = OwnProps;
 
-const App: FC<Props> = () => <div>Welcome to starter</div>;
+const App: FC<Props> = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <ContentWrapper>
+                <p>Welcome to my starter!</p>
+                <PrimaryButton text="Hello world" width="150px" />
+            </ContentWrapper>
+        </>
+    );
+};
+
+const ContentWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    height: 100vh;
+`;
 
 export default App;
