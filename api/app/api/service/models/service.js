3d7820a762278093270c01059f33f113fdcdb4a6
Andrzej Marek <a.marek@fireup.pro>
2020-11-21 17:37:14 +0100

    Createting uid with new service

--- api/app/api/service/models/service.js
+++ api/app/api/service/models/service.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const uuid = require("uuid");
+
+module.exports = {
+    lifecycles: {
+        // Called before an entry is created
+        beforeCreate(data) {
+            const randomNumber = uuid.v4();
+            data.service_id = randomNumber.substring(0, 8);
+        },
+    },
+};
