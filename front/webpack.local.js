961c659f3fa81ec99ab61f84f5811f28fbf27a04
Andrzej Marek <a.marek@fireup.pro>
2020-11-23 19:19:33 +0100

    files refactor

---
+++ front/webpack.local.js
@@ -0,0 +1,14 @@
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common(), {
+    mode: "development",
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                BACKEND_URL: JSON.stringify("http://localhost:1337"),
+            },
+        }),
+    ],
+});
