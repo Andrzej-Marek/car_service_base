9682b349d5941cb72840398b0a3def1f38e4cb70
Andrzej Marek <a.marek@fireup.pro>
2020-12-08 18:22:04 +0100

    Changed file structure

--- api/app/api/custom/controllers/Custom.js
+++ api/app/api/custom/controllers/Custom.js
@@ -8,4 +8,37 @@ module.exports = {
       message: "Successfully destroyed session",
     });
   },
+
+  async me(ctx) {
+    if (
+      ctx.request &&
+      ctx.request.header &&
+      !ctx.request.header.authorization
+    ) {
+      const token = ctx.cookies.get("token");
+      if (token) {
+        ctx.request.header.authorization = "Bearer " + token;
+      }
+    }
+
+    const user = await strapi.plugins[
+      "users-permissions"
+    ].services.jwt.getToken(ctx);
+
+    console.log("user", user);
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const userPayload = await strapi
+      .query("user", "users-permissions")
+      .findOne({
+        id: user.id,
+      });
+
+    ctx.send({ user: userPayload });
+  },
 };
---
+++ api/app/config/middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+  settings: {
+    cors: {
+      origin: [
+        "http://localhost:8080",
+        "http://localhost:1337",
+        "https://mysite.com",
+        "https://www.mysite.com",
+      ],
+    },
+  },
+};
