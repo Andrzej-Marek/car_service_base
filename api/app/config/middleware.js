a0927f4e9cd903ff1b17d11496379e2472b4f816
Andrzej Marek <a.marek@fireup.pro>
2020-12-09 21:30:37 +0100

    Added server Ip to origin

--- api/app/config/middleware.js
+++ api/app/config/middleware.js
@@ -8,6 +8,8 @@ module.exports = {
         "https://www.mysite.com",
         "http://193.178.43.128",
         "http://193.178.43.128:1337",
+        "http://raportserwisowy.pl",
+        "http://raportserwisowy.pl:1337",
       ],
     },
   },
