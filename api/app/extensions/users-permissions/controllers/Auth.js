c99b951ff196633deb7ae98aac2678343c150b23
Andrzej Marek <a.marek@fireup.pro>
2020-12-18 17:58:03 +0100

    next gixed

--- api/app/extensions/users-permissions/controllers/Auth.js
+++ api/app/extensions/users-permissions/controllers/Auth.js
@@ -133,12 +133,12 @@ module.exports = {
         const token = strapi.plugins["users-permissions"].services.jwt.issue({
           id: user.id,
         });
-
+        console.log("process.env", process.env);
         ctx.cookies.set("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production" ? true : false,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          domain: process.env.CLIENT_URL,
+          domain: process.env.CLIENT_DOMAIN,
         });
 
         ctx.send({
