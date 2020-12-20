14dabb60f0f999962bea8cf198523e95357fd603
Andrzej Marek <a.marek@fireup.pro>
2020-12-20 18:13:25 +0100

    cors

--- api/app/config/middleware.js
+++ api/app/config/middleware.js
@@ -1,19 +1,16 @@
 module.exports = {
   settings: {
     cors: {
-      // origin: [
-      //   "http://localhost:8080",
-      //   "http://localhost",
-      //   "http://193.178.43.128",
-      //   "https://193.178.43.128",
-      //   "http://raportserwisowy.pl",
-      //   "https://raportserwisowy.pl",
-      //   "https://api.raportserwisowy.pl",
-      //   "https://serwis.raportserwisowy.pl",
-      // ],
-      origin: "*",
-      enabled: true,
-      headers: "*",
+      origin: [
+        "http://localhost:8080",
+        "http://localhost",
+        "http://193.178.43.128",
+        "https://193.178.43.128",
+        "http://raportserwisowy.pl",
+        "https://raportserwisowy.pl",
+        "https://api.raportserwisowy.pl",
+        "https://serwis.raportserwisowy.pl",
+      ],
     },
   },
 };
