2b1303665d5edb5da433891afcfefc0a02611fc9
Andrzej Marek <a.marek@fireup.pro>
2021-01-04 21:34:14 +0100

    secure app

--- api/app/config/server.js
+++ api/app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: "https://api.raportserwisowy.pl",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "995e5be59c45db91ee8166b933be8e52"),
